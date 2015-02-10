bf64544516d970fb14b6236bdbfd8d53f52838b3
Anusha Shankar <nushiamme@gmail.com>
2015-02-10 17:05:01 -0500

    Added small code to plot species selections for Chordate

---
+++ Species_selection.R
@@ -0,0 +1,9 @@
+library(reshape)
+library(ggplot2)
+species <- read.csv("C:\\Users\\ANUSHA\\Desktop\\Species_taxa.csv")
+
+head(species)
+
+sp <- melt(species, measure.vars = "Category", na.rm=T)
+ggplot(species, aes(Category)) + geom_bar() + theme_bw() + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+  
