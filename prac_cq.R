db8112037edcafadc93edd79dd1bb4499b2be94c
Anusha Shankar <nushiamme@gmail.com>
2015-03-02 12:56:07 -0500

    Modified ggplots, fixed axes, separate axes for CQs and Prac totals. Much better now, yay

--- prac_cq.R
+++ prac_cq.R
@@ -1,12 +1,19 @@
 library(ggplot2)
 library(reshape)
+library(gridExtra)
 
 cq_prac <- read.csv("E:\\Toshiba_Desktop\\Stony Brook\\Teaching courses\\Chordate Spring 2015\\Lab Grades\\CQ_Prac_comparison.csv")
 
-m.cq <- melt(cq_prac, id.vars = "Section", measure.vars = c("CQ_Average","Prac1_total"))
+m.cq_prac <- melt(cq_prac, measure.vars = c("CQ_Average","Prac1_total"), na.rm=T)
 
-ggplot(m.cq, aes(variable,value)) + geom_boxplot() +  
-  facet_wrap(variable~Section,scales = "free_y") + theme_bw()
+m.cq <- m.cq_prac[m.cq_prac$variable=="CQ_Average",]
+m.prac <- m.cq_prac[m.cq_prac$variable=="Prac1_total",]
+cqPlot <- ggplot(m.cq, aes(variable,value)) + geom_boxplot(aes(fill = factor(Section))) +  
+  facet_wrap(variable~Section) + theme_bw() + theme(legend.position = "none") + 
+  scale_fill_brewer(type = "qual",palette = 1)
+pracPlot <- ggplot(m.prac, aes(variable,value)) + geom_boxplot(aes(fill = factor(Section))) +  
+  facet_wrap(variable~Section) + theme_bw() + theme(legend.position = "none") +
+  scale_fill_brewer(type = "qual",palette = 1)
 
-##ggplot(m.cq, aes(variable,value)) + geom_boxplot() +
-##  facet_wrap(variable~Section,scales="free_y") + coord_cartesian(ylim = c(0, 10)) + theme_bw()
+grid.arrange(cqPlot, pracPlot, nrow=2, ncol=1)
+
