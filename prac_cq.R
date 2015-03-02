f43ebfb25f85ea9b9204abc8aba3aa466316c4f7
Anusha Shankar <nushiamme@gmail.com>
2015-03-02 12:24:21 -0500

    Modified ggplot to have free y axis

--- prac_cq.R
+++ prac_cq.R
@@ -6,7 +6,7 @@ cq_prac <- read.csv("E:\\Toshiba_Desktop\\Stony Brook\\Teaching courses\\Chordat
 m.cq <- melt(cq_prac, id.vars = "Section", measure.vars = c("CQ_Average","Prac1_total"))
 
 ggplot(m.cq, aes(variable,value)) + geom_boxplot() +  
-  facet_wrap(variable~Section,scales="free_y") + theme_bw()
+  facet_wrap(variable~Section,scales = "free_y") + theme_bw()
 
-ggplot(m.cq, aes(variable,value)) + geom_boxplot() +
-  facet_wrap(variable~Section,scales="free_y") + coord_cartesian(ylim = c(0, 10)) + theme_bw()
+##ggplot(m.cq, aes(variable,value)) + geom_boxplot() +
+##  facet_wrap(variable~Section,scales="free_y") + coord_cartesian(ylim = c(0, 10)) + theme_bw()
