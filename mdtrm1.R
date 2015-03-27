b9fe5a9553ebcd2f57585175d61bed813a15cc16
Anusha Shankar <nushiamme@gmail.com>
2015-03-27 13:22:03 -0400

    added bw theme

--- mdtrm1.R
+++ mdtrm1.R
@@ -15,6 +15,6 @@ m.mid <- melt(midterm, id.vars=c("LastName", "Lab_Section"), measure.vars="Midte
 ## Create plot for midterm 
 MidtermPlot <- ggplot(m.mid[which(m.mid$value>0),], aes(Lab_Section,value)) + geom_boxplot(aes(fill = factor(Lab_Section))) +  
   scale_fill_brewer(type = "qual",palette = 1, guide = FALSE) + xlab("Midterm 1") + geom_point() +
-  ylab("Points")
+  ylab("Points") + theme_bw()
 MidtermPlot
 median(m.mid$value[m.mid$Lab_Section=="L02"])
