91471862ef556297e18a3632cbeab39cbf91a12b
Anusha Shankar <nushiamme@gmail.com>
2015-04-10 12:20:47 -0400

    Putting plots for pracs 1 and 2 together

--- prac_cq.R
+++ prac_cq.R
@@ -21,26 +21,53 @@ m.ta <- m.cq_prac[m.cq_prac$variable=="TA Questions",]
 m.cq2 <- m.cq_prac2[m.cq_prac2$variable=="CQ2",]
 m.ta2 <- m.cq_prac2[m.cq_prac2$variable=="TA2",]
 
-## Create plot for just CQ's
-cqPlot <- ggplot(m.cq2, aes(variable, as.numeric(value))) + geom_boxplot(aes(fill = factor(Section))) +  
+## Create plot for just CQ's - PRAC 1
+cqPlot <- ggplot(m.cq, aes(variable, as.numeric(value))) + geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") + 
-  scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs") + ylab("Points") +
+  scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs Prac 1") + ylab("Points") +
   scale_x_discrete(labels="") +
   theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
 
-## Create plot for total prac 
+## Create plot for just TA questions -  PRAC 1
 
-pracPlot <- ggplot(m.ta2, aes(variable, as.numeric(value))) + geom_boxplot(aes(fill = factor(Section))) +  
+pracPlot <- ggplot(m.ta, aes(variable, as.numeric(value))) + geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") +
-  scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions") + ylab("Points") +
+  scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions Prac 1") + ylab("Points") +
   scale_x_discrete(labels="") +
   theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
 
-## Plot both
+## Create plot for just CQ's - PRAC 2
+cqPlot2 <- ggplot(m.cq2, aes(variable, as.numeric(value))) + geom_boxplot(aes(fill = factor(Section))) +  
+  geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") + 
+  scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs Prac 2") + ylab("Points") +
+  scale_x_discrete(labels="") +
+  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+        axis.text.x = element_text(size=12, face="bold"),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
+  theme(strip.text.x = element_text(size = 14))
+
+## Create plot for total prac - PRAC 2
+
+pracPlot2 <- ggplot(m.ta2, aes(variable, as.numeric(value))) + geom_boxplot(aes(fill = factor(Section))) +  
+  geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") +
+  scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions Prac 2") + ylab("Points") +
+  scale_x_discrete(labels="") +
+  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+        axis.text.x = element_text(size=12, face="bold"),
+        axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
+  theme(strip.text.x = element_text(size = 14))
+
+## Plot both - PRAC 1
 grid.arrange(cqPlot, pracPlot, nrow=2, ncol=1)
+
+## Plot both - PRAC 2
+grid.arrange(cqPlot2, pracPlot2, nrow=2, ncol=1)
+
+## Plot PRAC 1 and PRAC 2
+grid.arrange(cqPlot, cqPlot2, pracPlot, pracPlot2, nrow=2, ncol=2)
