7923d2b91fc094c2f1b4d8ff537b83fa62bfdaeb
Anusha Shankar <nushiamme@gmail.com>
2015-11-18 10:54:18 -0500

    practice

---
+++ josh practice.R
@@ -0,0 +1,15 @@
+help.start()
+x <- rnorm (50)
+y <- rnorm (x)
+ls()
+rm (x,y)
+x <- 1:20
+w <- 1 + sqrt (x)/2
+dummy <- data.frame (x=x, y= x + rnorm (x) *w)
+dummy
+fm1 <-lm (y ~ x, data=dummy)
+summary (fm1)
+fm1<-lm(y~x,data=dummy,weight=1/w^2)
+attach(dummy)
+plot (x, y)
+lrf <- lowess (x,y)
