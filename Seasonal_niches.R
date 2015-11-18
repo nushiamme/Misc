2d4c7fd001b49dfa06adc5bad8028622ce5a2849
Anusha Shankar <nushiamme@gmail.com>
2015-11-18 00:33:06 -0500

    started code a long time ago, didn't push

---
+++ Seasonal_niches.R
@@ -0,0 +1,13 @@
+## Jan Engler et al. (in prep), Seasonal niches passerines
+## Anusha Shankar, July 8, 2015
+
+library(reshape)
+
+setwd("E:\\Toshiba_Desktop\\Stony Brook\\Catherine lab\\Jan")
+
+seas <- read.csv("Seasonal niches in new world migrating passerines.csv")
+
+birdtraits <- read.csv("Birdbase_subset.csv")
+
+??subset
+
