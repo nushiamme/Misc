0fbbc0b3e629e0a8240318d108bea55b8e08d491
Anusha Shankar <nushiamme@gmail.com>
2016-04-12 11:28:21 -0400

    Casey conducting data visualization seminar at Tuesday noon

---
+++ data_visualization.R
@@ -0,0 +1,8 @@
+## Test with Casey's data visualization tutorial
+## Anusha Shankar
+## April 12, 2016
+
+## Setwd and read in file
+setwd("C:\\Users\\ANUSHA\\Desktop\\")
+
+test <- read.csv("gapminder-FiveYearData.csv")
