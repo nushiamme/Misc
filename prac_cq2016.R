13850c583a0bfb494941d43e43bfc42e254ff94e
Anusha Shankar <nushiamme@gmail.com>
2016-04-21 18:16:00 -0400

    Added pracs 2 and 3, and updated NH code

--- prac_cq2016.R
+++ prac_cq2016.R
@@ -4,24 +4,25 @@ library(gridExtra)
 library(ggmap)
 
 ## Set wd and Read in csv
-setwd("E:\\Toshiba_Desktop\\Stony Brook\\Teaching courses\\Chordate Spring 2016\\HeadTA_grading\\Lab")
-cq_prac <- read.csv("CQ_Prac_comparison2.csv")
+setwd("E:\\Toshiba_Desktop\\Stony Brook\\Teaching courses\\Chordate Spring 2016\\HeadTA_grading")
+cq_prac <- read.csv("Lab\\CQ_Prac_comparison2.csv")
 cq_prac2 <- read.csv("CQ_Prac2_comparison.csv",stringsAsFactors=F)
-names(cq_prac) <- c("Section", "CQ", "TA Questions", "Prac1 Total")
-names(cq_prac2) <- c("Section", "CQ1", "Prac1 Total", "TA1", "CQ2", 
+#names(cq_prac) <- c("Section", "CQ", "TA Questions", "Prac1 Total")
+#names(cq_prac2) <- c("Section", "CQ1", "Prac1 Total", "TA1", "CQ2", 
                      "Prac2 Total", "TA2", "CQ3", "Prac3 Total", "TA3")
-
 nhpaper <- read.csv("NHpaper.csv")
 
 ## Melt
-m.cq_prac <- melt(cq_prac, measure.vars = c("CQ","Prac1 Total","TA Questions"), na.rm=T)
-m.cq_prac2 <- melt(cq_prac2, id.vars= "Section",measure.vars = c("CQ1","TA1","CQ2", "TA2","CQ3", "TA3"), na.rm=T)
+m.cq_prac <- melt(cq_prac, id.vars="Section", 
+                  measure.vars = c("CQ1","TA1","CQ2", "TA2","CQ3", "TA3"), na.rm=T)
+m.cq_prac2 <- melt(cq_prac2, id.vars= "Section",
+                   measure.vars = c("CQ1","TA1","CQ2", "TA2","CQ3", "TA3"), na.rm=T)
 
-m.nhpaper <- melt(nhpaper, id.vars="Grading_TA", measure.vars="NaturalHistory_Grade", na.rm=T)
+m.nhpaper <- melt(nhpaper, id.vars="Grader", measure.vars="Total", na.rm=T)
 
 ##Separate CQ and prac
-m.cq <- m.cq_prac[m.cq_prac$variable=="CQ",]
-m.ta <- m.cq_prac[m.cq_prac$variable=="TA Questions",]
+m.cq <- m.cq_prac[m.cq_prac$variable %in% c("CQ1","CQ2", "CQ3"),]
+m.ta <- m.cq_prac[m.cq_prac$variable %in% c("TA1","TA2","TA3"),]
 
 m.cq2 <- m.cq_prac2[m.cq_prac2$variable=="CQ2",]
 m.ta2 <- m.cq_prac2[m.cq_prac2$variable=="TA2",]
@@ -29,63 +30,76 @@ m.ta2 <- m.cq_prac2[m.cq_prac2$variable=="TA2",]
 m.cq3 <- m.cq_prac2[m.cq_prac2$variable=="CQ3",]
 m.ta3 <- m.cq_prac2[m.cq_prac2$variable=="TA3",]
 
-## Create plot for just CQ's - PRAC 1
-cqPlot <- ggplot(m.cq, aes(variable, as.numeric(as.character(value)))) + geom_boxplot(aes(fill = factor(Section))) +  
+## Create plot for just CQ's
+cqPlot <- ggplot(m.cq, aes(variable, as.numeric(as.character(value)))) + 
+  geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") + 
-  scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs Prac 1") + ylab("Points") +
-  scale_x_discrete(labels="") +
-  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+  scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs Questions") + ylab("Points") +
+ # scale_x_discrete(labels="") +
+  theme(text=element_text(family="sans"), 
+        axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
+cqPlot
 
-## Create plot for just TA questions -  PRAC 1
-
-pracPlot <- ggplot(m.ta, aes(variable, as.numeric(as.character(value)))) + geom_boxplot(aes(fill = factor(Section))) +  
+## Create plot for just TA questions
+pracPlot <- ggplot(m.ta, aes(variable, as.numeric(as.character(value)))) + 
+  geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") +
-  scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions Prac 1") + ylab("Points") +
-  scale_x_discrete(labels="") +
-  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+  scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions") + ylab("Points") +
+  #scale_x_discrete(labels="") +
+  theme(text=element_text(family="sans"), 
+        axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
+pracPlot
 
 ## Create plot for just CQ's - PRAC 2
-cqPlot2 <- ggplot(m.cq2, aes(variable, as.numeric(as.character(value)))) + geom_boxplot(aes(fill = factor(Section))) +  
+cqPlot2 <- ggplot(m.cq2, aes(variable, as.numeric(as.character(value)))) + 
+  geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") + 
   scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs Prac 2") + ylab("Points") +
   scale_x_discrete(labels="") +
-  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+  theme(text=element_text(family="sans"), 
+        axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
 
 ## Create plot for TA prac - PRAC 2
-pracPlot2 <- ggplot(m.ta2, aes(variable, as.numeric(as.character(value)))) + geom_boxplot(aes(fill = factor(Section))) +  
+pracPlot2 <- ggplot(m.ta2, aes(variable, as.numeric(as.character(value)))) + 
+  geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") +
   scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions Prac 2") + ylab("Points") +
   scale_x_discrete(labels="") +
-  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+  theme(text=element_text(family="sans"), 
+        axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
 
 ## Create plot for just CQ's - PRAC 3
-cqPlot3 <- ggplot(m.cq3, aes(variable, as.numeric(as.character(value)))) + geom_boxplot(aes(fill = factor(Section))) +  
+cqPlot3 <- ggplot(m.cq3, aes(variable, as.numeric(as.character(value)))) + 
+  geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") + 
   scale_fill_brewer(type = "qual",palette = 1) + xlab("CQs Prac 3") + ylab("Points") +
   scale_x_discrete(labels="") +
-  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+  theme(text=element_text(family="sans"), 
+        axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
 
 ## Create plot for TA prac - PRAC 3
-pracPlot3 <- ggplot(m.ta3, aes(variable, as.numeric(as.character(value)))) + geom_boxplot(aes(fill = factor(Section))) +  
+pracPlot3 <- ggplot(m.ta3, aes(variable, as.numeric(as.character(value)))) + 
+  geom_boxplot(aes(fill = factor(Section))) +  
   geom_point() + facet_wrap(~Section, nrow=1) + theme_bw() + theme(legend.position = "none") +
   scale_fill_brewer(type = "qual",palette = 1) + xlab("TA questions Prac 3") + ylab("Points") +
   scale_x_discrete(labels="") +
-  theme(text=element_text(family="sans"), axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
+  theme(text=element_text(family="sans"), 
+        axis.title.x = element_text(size=16, vjust=0.2, face="bold"), 
         axis.text.x = element_text(size=12, face="bold"),
         axis.title.y = element_text(size=16, face="bold"), axis.text.y = element_text(size=12)) +
   theme(strip.text.x = element_text(size = 14))
@@ -100,5 +114,6 @@ grid.arrange(cqPlot2, pracPlot2, nrow=2, ncol=1)
 grid.arrange(cqPlot, cqPlot2, cqPlot3, pracPlot, pracPlot2, pracPlot3, nrow=2, ncol=3)
 
 ## Plot for natural history paper
-nhplot <- ggplot(m.nhpaper, aes(Grading_TA, value)) + geom_boxplot() + geom_point() + theme_bw()
+nhplot <- ggplot(m.nhpaper, aes(Grader, value), na.omit=T) + geom_boxplot() + 
+  geom_point() + theme_bw(base_size = 30) + ylab("Points")
 nhplot
