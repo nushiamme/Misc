51eea5406d6fb3666060a88991852af5261d5c68
Anusha Shankar <nushiamme@gmail.com>
2018-04-12 13:37:56 -0400

    code for torpor bird filtering

--- Color_filtering_from_images.R
+++ Color_filtering_from_images.R
@@ -1,6 +1,9 @@
 # Run this in the shell with Image Magick installed to remove certain colors 
 # in this case black and 10% fuzz for similar colors
 
-#convert BlueThroated_hummingbird_peeing_crop.jpg -channel rgba -alpha set -fuzz 10% -fill none -opaque black BLUH_Pee_crop_test.png
+#convert respirometry_graph.jpg -channel rgba -alpha set -fuzz 10% -fill none -opaque cyan respir_test.png
 
 #convert SaveMyTime_sample_graph.jpeg -channel rgba -alpha set -fuzz 10% -fill none -opaque white SaveMyTime_sample_graph_transp.jpeg
+
+#To transform cyan to red and save into test folder
+#mogrify .\Desktop\test -format png -fill red -fuzz 20% -opaque cyan *.png
